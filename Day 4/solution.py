9a920c61736c672fd2e7f6c784c82c6fcb377066
GuilhermeColla <guilherme.colla.159@gmail.com>
2020-12-05 13:42:45 -0300

    Day 4 part 1 solution.

--- Day 4/solution.py
+++ Day 4/solution.py
@@ -15,25 +15,42 @@ with open('Day 4/example_input', 'r') as f:
 with open('Day 4/puzzle_input') as f:
 	puzzle_input = [x.strip('\n') for x in f]
 # TODO: utilizar um stack para juntar os elementos da lista.
-i = 0
-parsed = list()
-while i < len(example_input)-2:
-    if (example_input[i] != '') and (example_input[i+1] != ''):
-        parsed.append(example_input[i] + ' ' + example_input[i+1])
-    i += 1
-print(parsed)
-print(example_input)
+
 example = 1
+
+# It returns a list with each passport as an string element inside a list.
+def data_parse(data):
+    stk = list()
+    parsed = list()
+    for element in data:
+        aux = ''
+        if element != '':
+            stk.append(element)
+        elif element == '':
+            while stk:
+                aux += stk.pop() + ' '
+            parsed.append(aux)
+    while stk:
+        aux += stk.pop() + ' '
+    parsed.append(aux)
+    return parsed
+                
 
 def part1(data):
     valid = 0
-    pattern = '(byr)|(iyr)|(eyr)|(hgt)|(hcl)|(ecl)|(pid)'
+    full_pattern = ['(byr)', '(iyr)', '(eyr)', '(hgt)', '(hcl)', '(ecl)', '(pid)']
     for element in data:
-        if element != '':
-            pass
+        passed_tests = 0
+        for pattern in full_pattern:
+            if re.findall(pattern, element):
+                passed_tests +=1
+        if passed_tests == len(full_pattern):
+            valid +=1
+    return valid
 
 
 if __name__ == "__main__":
     if example:
-        # assert part1(example_input) == 2
+        assert part1(data_parse(example_input)) == 2
         print('Correct result on part 1 example!')
+print(f'The answer to part 1 is: {part1(data_parse(puzzle_input))}')
