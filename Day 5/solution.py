9bc5a309689e842fb43346494e5c11a1d1e99854
GuilhermeColla <guilherme.colla.159@gmail.com>
2020-12-06 12:11:46 -0300

    Day 5 part 2 complete!

--- Day 5/solution.py
+++ Day 5/solution.py
@@ -5,10 +5,11 @@ with open('Day 5/example_input', 'r') as f:
 with open('Day 5/puzzle_input', 'r') as f:
     puzzle_input = [x.strip('\n') for x in f]
 
-example = 1
+example = 0
 
 def part1(data):
     ID = 0
+    available = {k:[v for v in range (8)] for k in range (128)} # this is for part 2
     for element in data:
         row = [x for x in range(128)]
         col = [x for x in range(8)]
@@ -21,11 +22,16 @@ def part1(data):
                 col = col[int(len(col)/2):]
             elif char == 'L':
                 col = col[:int(len(col)/2)]
-        if ID < row[0] * 8 + col[0]:
+        available[row[0]].remove(col[0]) # removing occupied places from the available dict.
+        if ID < row[0] * 8 + col[0]:    # grabbing the highest ID.
             ID = row[0] * 8 + col[0]
-    return ID
+    for key,v in available.items():
+        if len(v) == 1:
+           part_2_ID = key * 8 + v[0] 
+    return [ID, part_2_ID]
 
 if __name__ == "__main__":
     if example:
         print(part1(example_input))
-    print(f'The answer to part 1 is: {part1(puzzle_input)}')
+    print(f'The answer to part 1 is: {part1(puzzle_input)[0]}')
+    print(f'The answer to part 2 is: {part1(puzzle_input)[1]}')
